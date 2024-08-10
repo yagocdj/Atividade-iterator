1bc378dc587618a3d95ab9d0d432451c4d433e6f
Yago César <yagocdj@gmail.com>
2024-08-10 20:32:25 -0300

    finalizando o iterator em pré ordem

--- colaborativaum/src/br/edu/ifpb/pps/colaborativaum/iterator/PreorderIterator.java
+++ colaborativaum/src/br/edu/ifpb/pps/colaborativaum/iterator/PreorderIterator.java
@@ -2,56 +2,31 @@ package br.edu.ifpb.pps.colaborativaum.iterator;
 
 import br.edu.ifpb.pps.colaborativaum.tree.Node;
 
-import java.util.Stack;
+import java.util.*;
 
 
 public class PreorderIterator<T extends Comparable<T>> implements BSTIterator<T> {
 
-    private Node<T> currentNode;
-    private Stack<Node<T>> nodeStack;
+    private final Deque<Node<T>> stack;
 
     public PreorderIterator(Node<T> root) {
-        this.currentNode = root;
+        stack = new ArrayDeque<>();
+        if (root != null) {
+            stack.push(root);
+        }
     }
 
     @Override
-    public Node<T> getNext() {
-        return null;
+    public T getNext() {
+        Node<T> node = stack.pop();
+        if (node.right != null) stack.push(node.right);
+        if (node.left != null) stack.push(node.left);
+        return node.value;
     }
 
     @Override
     public boolean hasNext() {
-        return false;
+        return !stack.isEmpty();
     }
 
-    public void iterativePreorder(Node<T> node) {
-
-        // Base case
-        if (node == null) return;
-
-        // Create an empty stack and push root to it
-        // Stack<Node<T>> nodeStack = new Stack<Node<T>>();
-        nodeStack.push(currentNode);  // currentNode starts at root
-
-        /* Pop all items one by one. Do following for every popped item
-         a) print it
-         b) push its right child
-         c) push its left child
-         Note that right child is pushed first so that left is processed first */
-        while (!nodeStack.empty()) {
-
-            // Pop the top item from stack and print it
-            Node<T> currentNode = nodeStack.peek();  // gets the element without removing it
-            System.out.print(currentNode.value + " ");
-            // nodeStack.pop();  // removes the element from the stack
-
-            // Push right and left children of the popped node to stack
-            if (currentNode.right != null) {
-                nodeStack.push(currentNode.right);
-            }
-            if (currentNode.left != null) {
-                nodeStack.push(currentNode.left);
-            }
-        }
-    }
 }
